fef2b514c65cd54945b00a17ee5d6f6ac6699d8f
Dexter◥▶_◀◤ <335ishwar@gmail.com>
2025-07-23 11:17:33 +0530

    Netflix Clone using React

---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the Netflix Clone</h1>
+      <p>Enjoy your favorite movies and shows!</p>
+    </div>
+  );
+}
---
+++ src/components/Pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";  
+import "./Login.css";
+function Login() {
+  return (
+    <div className="login">
+      <h1>Login Page</h1>
+      <p>Please enter your credentials to log in.</p>
+    </div>
+  );
+}
+default export Login;
